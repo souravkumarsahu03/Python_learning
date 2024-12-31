8756f5b7d186720582bf531659095208f58e269b
Sourav Kumar Sahu <119477508+souravkumarsahu03@users.noreply.github.com>
2024-12-31 15:11:29 +0000

    basic py done

--- count_Vowels.py
+++ count_Vowels.py
@@ -4,4 +4,4 @@ for i in a:
     if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u' or i == 'A'or i == 'E' or i == 'I' or i == 'O' or i == 'U':
         vowels += 1
     
-print("Number of voeels are : ", vowels)
+print("Number of vowels are : ", vowels)
